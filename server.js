0e9e53a40adaf7799c72611d7513775d5d0921c5
sajjal <nairsajjal@gmail.com>
2019-06-26 16:17:39 +0530

    connection established

--- server.js
+++ server.js
@@ -4,9 +4,19 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const PORT = 4000;
 
+
+const mongoose = require('mongoose');
+
 app.use(cors());
 app.use(bodyParser.json());
 
-app.listen(PORT, function(){
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser : true});
+const connection = mongoose.connection;
+
+connection.once('open', function(){
+    console.log("Mongodb connection established");
+});
+
+app.listen(PORT, function(){  
     console.log("Server started on 4000");
 });
