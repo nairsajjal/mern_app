8cf47418540975b9d3aa2557570b0f4bb3001d8e
sajjal <nairsajjal@gmail.com>
2019-06-27 10:27:42 +0530

    final commit

--- server.js
+++ server.js
@@ -49,6 +49,26 @@ todoRoutes.route('/add').post(function(req,res){
     })
 });
 
+
+todoRoutes.route('/update/:id').post(function(req,res){
+    Todo.findById(req.param.id, function(err, todo){
+            if(!todo)
+            res.status(404).send('data is not found');
+            else
+            todo.todo_description = req.body.todo_description;
+            todo.todo_responsible = req.body.todo_responsible;
+            todo.todo_priority = req.body.todo_priority;
+            todo.todo_completed = req.body.todo_completed;
+
+            todo.save().then(todo => {
+                res.json('Todo updated');
+            })
+            .catch(err => {
+                res.status(404).send("Update Not possible");
+            });
+    });
+});
+
 app.use('/todos', todoRoutes);
 
 app.listen(PORT, function(){  
